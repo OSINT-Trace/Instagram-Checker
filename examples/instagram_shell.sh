c9c3931ff2855477af2e3eadd93a22ee35ad526d
otmangx <otmangx@gmail.com>
2025-03-05 21:33:55 +0000

    Feat: Instagram Account Checker

---
+++ examples/instagram_shell.sh
@@ -0,0 +1,6 @@
+curl --request POST \
+	--url https://instagram-checker.p.rapidapi.com/check_bulk \
+	--header 'Content-Type: application/json' \
+	--header 'x-rapidapi-host: instagram-checker.p.rapidapi.com' \
+	--header 'x-rapidapi-key: Sign Up for Key' \
+	--data '{"input":["test@gmail.com","john@doe.com","19295551234"]}'
